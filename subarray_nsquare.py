82c30b4670cce2a69fd1b465fcde43479be4a0ec
Andrew <andrewmccann88@gmail.com>
2016-02-05 00:45:21 -0800

    Testing things that don't work.

--- subarray_nsquare.py
+++ subarray_nsquare.py
@@ -39,6 +39,45 @@ def max_subarray(array):
 
 max_subarray(array)
 
+
+def merge_subarray(array):
+    if len(array) == 1:
+        return array[0]
+
+    mid = len(array)//2
+    l_array = array[:mid]
+    r_array = array[mid:]
+
+    l_max = merge_subarray(l_array)
+    r_max = merge_subarray(r_array)
+
+    current_val = 0
+
+    if array[0] > 0:
+        current_val += array[0]
+    if l_max > 0:
+        current_val += l_max
+    if r_max > 0:
+        current_val += r_max
+
+
+
+    # Fuck all this shit below
+    current_max = array[0]
+
+    if current_max + l_max > l_max:
+        current_max += l_max
+    if current_max + r_max > r_max:
+        current_max += r_max
+    if current_max < l_max:
+        current_max = l_max
+    if current_max < r_max:
+        current_max = r_max
+
+
+
+
+
 # Need to brute force, so that means testing every single possible sub string.
 
 
