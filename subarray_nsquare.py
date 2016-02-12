eb1870428532e4d9eb83b1657587c5764c1195f0
Andrew M <andrewmccann88@gmail.com>
2016-02-11 23:54:37 -0800

    Started working on nlogn solution.

--- subarray_nsquare.py
+++ subarray_nsquare.py
@@ -7,13 +7,14 @@ Algorithms & Complexity
 CS350
 """
 
-NUM_ELEMENTS = 100
+NUM_ELEMENTS = 10
 
 array = []
 for i in range(NUM_ELEMENTS):
     array.append(random.randint(-10, 10))
 
 print(array)
+
 
 
 
@@ -27,22 +28,24 @@ def max_subarray(array):
                 max_substring = current_subarray
     print(max_substring)
 
-max_subarray(array)
+
+
 
 
 def merge_subarray(array):
     if len(array) == 1:
-        return array[0]
-
+        # Return the max value and the array
+        return array[0], array
+    # Split the array using // to make sure its an integer line.
     mid = len(array)//2
     l_array = array[:mid]
     r_array = array[mid:]
-
+    # Have great faith that this will work.
+    # Ideally it will return the maximum sub array.
     l_max = merge_subarray(l_array)
     r_max = merge_subarray(r_array)
 
     current_val = 0
-
     if array[0] > 0:
         current_val += array[0]
     if l_max > 0:
@@ -65,9 +68,41 @@ def merge_subarray(array):
         current_max = r_max
 
 
+# Pulled Mergesort from Interactivepython.org
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 
 
-# Need to brute force, so that means testing every single possible sub string.
+
+max_subarray(array)
 
 
